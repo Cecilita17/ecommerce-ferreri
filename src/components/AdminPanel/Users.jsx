7df8337052caf560b1428bdbed4bccb3a7334f6e
Cecilita17 <103078202+Cecilita17@users.noreply.github.com>
2023-07-12 22:28:10 -0300

    added profile picture to users list

--- src/components/AdminPanel/Users.jsx
+++ src/components/AdminPanel/Users.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { db } from './../../service/firebase/index';
 import { collection, getDocs } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { getStorage } from "firebase/storage";
+import "./Users.css"
+
 
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [usersF, setUsersF] = useState([]);
   const [mergedUsers, setMergedUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const storage = getStorage();
+
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -17,7 +23,7 @@ const Users = () => {
         setLoading(false);
       } catch (error) {
         console.log('Error fetching user data:', error);
-        setLoading(true); // Set loading to false even in case of error
+        setLoading(true);
       }
     };
 
@@ -33,7 +39,7 @@ const Users = () => {
         setLoading(false);
       } catch (error) {
         console.log('Error fetching users from Firestore:', error);
-        setLoading(true); // Set loading to false even in case of error
+        setLoading(true);
       }
     };
 
@@ -41,38 +47,53 @@ const Users = () => {
   }, []);
 
   useEffect(() => {
-    const mergeUsers = () => {
-      const merged = mergeArrays(usersF, users);
+    const mergeUsers = async () => {
+      const merged = await mergeArrays(usersF, users);
       setMergedUsers(merged);
     };
 
     mergeUsers();
   }, [users, usersF]);
 
-  const mergeArrays = (arr1, arr2) => {
-    const merged = arr1.map((item1) => {
-      const matchingItem = arr2.find((item2) => item2.uid === item1.uid);
-      if (matchingItem) {
+  const mergeArrays = async (arr1, arr2) => {
+    const merged = await Promise.all(
+      arr1.map(async (item1) => {
+        const matchingItem = arr2.find((item2) => item2.uid === item1.uid);
+        if (matchingItem) {
+          const photoURL = await getDownloadURL(ref(storage, matchingItem.uid));
+          console.log(photoURL)
+          return {
+            uid: item1.uid,
+            firstName: item1.firstName || '',
+            email: matchingItem.email || '',
+            photoURL: photoURL || '',
+          };
+        }
         return {
-          uid: item1.uid,
+          uid: item1.uid || '',
           firstName: item1.firstName || '',
-          email: matchingItem.email || '',
+          email: 'items dont match',
+          photoURL: '',
         };
-      }
-      return {
-        uid: item1.uid || '',
-        firstName: item1.firstName || '',
-        email: 'items dont match',
-      };
-    });
-  
+      })
+    );
+
     return merged;
   };
-  
-  
-  
 
-  console.log(mergedUsers);
+  const uploadProfilePicture = async (file, user) => {
+    const imageName = user.uid;
+    const fileRef = ref(storage, imageName);
+    await uploadBytes(fileRef, file);
+    const photoURL = await getDownloadURL(fileRef);
+
+    // Store the photoURL in the user's data in Firestore or update the respective user's document with the photoURL
+    // Example: db.collection('users').doc(user.uid).update({ photoURL });
+
+    console.log(`Uploaded profile picture for user ${user.uid}`);
+    console.log('Photo URL:', photoURL);
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -82,9 +103,10 @@ const Users = () => {
       <h1>User List</h1>
       <ul>
         {mergedUsers.map((user) => (
-          <li key={user.uid}>
+          <li className='usersLi' key={user.uid}>
+            <img className='usersImg' src={user.photoURL} alt="Profile" />
             <p>Email: {user.email}</p>
-            <p>First Name: {user.firstName}</p>
+            <p>Name: {user.firstName}</p>
           </li>
         ))}
       </ul>
@@ -93,4 +115,3 @@ const Users = () => {
 };
 
 export default Users;
-
